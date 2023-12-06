cd2c84618da6bca66ede2780b3b6e44c97fa18d5
Ruth <ruro122020@gmail.com>
2023-12-06 13:03:27 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+('Mr. Chocolate', 8, 'F', 'brown', 'active', 1),
+('Rowdy', 1, 'M', 'white', 'chill', 1),
+('Tabitha', 12, 'F', 'green', 'easy-going', 1),
+('Sergeant Brown', 4, 'F', 'purple', 'active', 1),
+('Melissa', 5, 'M', 'grey', 'super active', 0),
+('Grinch', 7, 'M', 'black', 'relaxed', 0),
+('Wendy', 7, 'F', 'black', 'super chill', 0),
+(Null, Null, 'M', 'blue', 'timid', 1);
